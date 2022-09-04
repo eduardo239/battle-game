b9bc0f2080cc09857dfbd5b97a8b78e7c85514f3
eduardo239 <eucrieumaconta@gmail.com>
2022-09-04 16:04:08 -0300

    Updates

---
+++ precache-manifest.a942faeaa3f160f623535292038e86c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f6b52cf51aaeed33313",
+    "url": "/card-game/static/css/main.1a1dddb8.chunk.css"
+  },
+  {
+    "revision": "4f6b52cf51aaeed33313",
+    "url": "/card-game/static/js/main.26851dc6.chunk.js"
+  },
+  {
+    "revision": "ebc24405c5239c99a5a5",
+    "url": "/card-game/static/js/runtime~main.d44b1061.js"
+  },
+  {
+    "revision": "840ff1fd27f6b5c2aa4e",
+    "url": "/card-game/static/js/2.81d6d09b.chunk.js"
+  },
+  {
+    "revision": "910a2acaa9cec9ca856a4d6d055b86dc",
+    "url": "/card-game/index.html"
+  }
+];
